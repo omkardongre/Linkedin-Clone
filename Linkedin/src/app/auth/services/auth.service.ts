bd6f5767e18fd3006da3488ddc212a7da2313ff6
omkardongre <omkardongre5@gmail.com>
2024-09-22 20:36:28 +0530

    refactor(frontend): migrate from capacitor/preference
    Replaced deprecated @capacitor/storage with @capacitor/preferences
    in auth-interceptor.service.ts and auth.service.ts.

--- Linkedin/src/app/auth/services/auth.service.ts
+++ Linkedin/src/app/auth/services/auth.service.ts
@@ -15,7 +15,7 @@ import {
   take,
   tap,
 } from "rxjs";
-import { Storage } from "@capacitor/storage";
+import { Preferences } from "@capacitor/preferences";
 import { UserResponse } from "../models/userResponse.model";
 import { jwtDecode } from "jwt-decode";
 import { ErrorHandlerService } from "src/app/core/error.handler.service";
@@ -79,7 +79,7 @@ export class AuthService {
       .pipe(
         take(1),
         tap(async (response: { token: string }) => {
-          await Storage.set({ key: "token", value: response.token });
+          await Preferences.set({ key: "token", value: response.token });
           const decodedToken: UserResponse = jwtDecode(response.token);
           this.$user.next(decodedToken.user);
           this.errorHandlerService.presentSuccessToast("User logged in");
@@ -105,12 +105,12 @@ export class AuthService {
 
   logout() {
     this.$user.next(null);
-    Storage.remove({ key: "token" });
+    Preferences.remove({ key: "token" });
   }
 
   //Check on app start if token is in storage
   isTokenInStorage(): Observable<boolean> {
-    return from(Storage.get({ key: "token" })).pipe(
+    return from(Preferences.get({ key: "token" })).pipe(
       take(1),
       switchMap((data) => {
         if (!data.value) {
