84bf8dff715bc703d933df0b7ba1a2ad953f1d66
omkardongre <omkardongre5@gmail.com>
2024-09-23 14:30:34 +0530

    chore(frontend): Format codebase after linting and formatting
    Adjusted existing code to adhere to the updated linting and Prettier rules.

--- Linkedin/src/app/home/components/advertising/advertising.component.ts
+++ Linkedin/src/app/home/components/advertising/advertising.component.ts
@@ -1,17 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
 
 @Component({
-  selector: 'app-advertising',
-  templateUrl: './advertising.component.html',
-  styleUrls: ['./advertising.component.scss'],
+  selector: "app-advertising",
+  templateUrl: "./advertising.component.html",
+  styleUrls: ["./advertising.component.scss"],
   imports: [IonicModule],
   standalone: true,
 })
-export class AdvertisingComponent  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class AdvertisingComponent implements OnInit {
+  constructor() {}
 }
