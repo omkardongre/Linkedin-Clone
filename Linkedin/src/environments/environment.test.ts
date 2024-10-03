b4362754289ec1fe22b6c7a075163819e1b049cc
omkardongre <omkardongre5@gmail.com>
2024-10-04 00:43:00 +0530

    test(frontend): Add Cypress setup and initial authentication tests
    Added Cypress configuration and support files for end-to-end testing.
    Implemented a custom task to clear the test database (clear-test-db.ts).
    Created initial authentication tests in auth.cy.ts.
    Added user fixture data in cypress/fixtures/user.json.
    Configured test setup with before hook in to clear the test database
    before tests.
    Added environment-specific configuration files for production,
    testing, and example setups.

---
+++ Linkedin/src/environments/environment.test.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  baseApiUrl: "http://localhost:3000/api",
+  dbConfig: {
+    host: "localhost",
+    port: 5434,
+    user: "postgres",
+    password: "password",
+    database: "linkedintest",
+  },
+};
