13128423beba5c7e05a2ae37c0e1b277e03bbf1b
omkardongre <omkardongre5@gmail.com>
2024-10-03 19:59:17 +0530

    fix: Fix lint issue

--- Linkedin/capacitor.config.ts
+++ Linkedin/capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'Linkedin',
-  webDir: 'www'
+  appId: "com.example.app",
+  appName: "Linkedin",
+  webDir: "www",
 };
 
 export default config;
