d78c7f71b6407b959cd12533c959e6861e8e6fd0
omkardongre <omkardongre5@gmail.com>
2024-10-07 13:08:47 +0530

    fix(frontend): remove unnecessary error handling for empty posts in feed

--- Linkedin/src/app/home/services/post.service.ts
+++ Linkedin/src/app/home/services/post.service.ts
@@ -43,11 +43,6 @@ export class PostService {
     return this.http
       .get<Post[]>(`${environment.baseApiUrl}/feed${queryParams}`)
       .pipe(
-        tap((posts: Post[]) => {
-          if (posts.length === 0) {
-            throw new Error("No posts found");
-          }
-        }),
         catchError(
           this.errorHandlerService.handleError<Post[]>("getSelectedPosts", []),
         ),
